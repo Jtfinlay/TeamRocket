4463ac9b6e6557f27b3fe3076b8488f5fc95b8c1
James Finlay <jtfinlay@ualberta.ca>
2014-10-20 10:14:53 -0600

    psim file added

---
+++ Assn2/psim.py
@@ -0,0 +1,23 @@
+#
+# CMPUT 313 - Computer Networks
+# Assignment 2 - Comparison of Medium Access Control Protocols
+#
+# Author: Jesse Tucker & James Finlay
+#
+
+#
+# @param Protocol:
+# 'T' - Time Division Multiplexing
+# 'P' - Slotted ALOHA with probabilistic backoff
+# 'I' - Slotted ALOHA with interval-based back-off
+# 'B' - Slotted ALOHA with a binary exponential back-off
+# @param N: Number of stations
+# @param p: Frame generation probability for each node
+# @param R: Total number of slots to simulate
+# @param T: Number of trials, followed by seeds of file
+def psim(Protocol, N, p, R, T):
+  print "Shit goes here."
+
+
+
+psim('T', 20, .01, 50000, [5, 1,2,3,4])
